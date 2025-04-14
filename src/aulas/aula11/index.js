60ef4e3b10467e5d22e34594d5eed99e1ff426db
rewrit3 <adolfo@rewrite.com.br>
2025-04-14 08:50:36 -0400

    algoritmo: aula 11

--- src/aulas/aula11/index.js
+++ src/aulas/aula11/index.js
@@ -38,12 +38,27 @@ function contarNulosArray(valores) {
   let contador = 0;
 
   for (const valor of valores) {
-    if (valor === null) {
+    if (valor === 0) {
       contador++;
     }
   }
 
   return contador;
+}
+
+function contarSomaParesArray(valores) {
+  const numerosPares = [];
+  let somaPares = 0;
+
+  for (let i = 0; i < valores.length; i++) {
+    if (valores[i] % 2 === 0) {
+      numerosPares.push(valores[i]);
+
+      somaPares += valores[i];
+    }
+  }
+
+  return somaPares;
 }
 
 const valor1 = parseInt(prompt('Digite o 1º valor: '));
@@ -53,16 +68,16 @@ const valor4 = parseInt(prompt('Digite o 4º valor: '));
 const valor5 = parseInt(prompt('Digite o 5º valor: '));
 
 const soma = valor1 + valor2 + valor3 + valor4 + valor5;
-
 const media = (valor1 + valor2 + valor3 + valor4 + valor5) / 5;
 
-const valoresDivisiveis = [valor1, valor2, valor3, valor4, valor5];
-const quantidadeDivisiveis = contarDivisiveisPor5Array(valoresDivisiveis);
+const valoresArray = [valor1, valor2, valor3, valor4, valor5];
 
-const valoresNulos = [valor1, valor2, valor3, valor4, valor5];
-const quantidadeNulos = contarDivisiveisPor5Array(valoresNulos);
+const quantidadeDivisiveis = contarDivisiveisPor5Array(valoresArray);
+const quantidadeNulos = contarNulosArray(valoresArray);
+const somaPares = contarSomaParesArray(valoresArray);
 
 console.log('A soma entre os valores é: ' + soma);
 console.log('A média entre os valores é: ' + media);
 console.log('A quantidade de valores divisiveis por 5 é: ' + quantidadeDivisiveis);
 console.log('A quantidade de valores nulos é: ' + quantidadeNulos);
+console.log('A quantidade de valores nulos é: ' + somaPares);
