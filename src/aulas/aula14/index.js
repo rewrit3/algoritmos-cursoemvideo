abf5e62b0c83834da335cac1047c07cf94030205
rewrit3 <adolfo@rewrite.com.br>
2025-04-16 09:53:00 -0400

    algoritmo: aula 14 (continua)

--- src/aulas/aula14/index.js
+++ src/aulas/aula14/index.js
@@ -19,38 +19,70 @@
 // console.log(valoresPares);
 
 // prática 1: listagem de notas da turma
-const nomesAlunos = [];
-const notas1 = [];
-const notas2 = [];
-const mediasAlunos = [];
-let mediasSoma = 0;
-let mediaTurma = 0;
-let alunosAcimaMedia = 0;
+// const nomesAlunos = [];
+// const notas1 = [];
+// const notas2 = [];
+// const mediasAlunos = [];
+// let mediasSoma = 0;
+// let mediaTurma = 0;
+// let alunosAcimaMedia = 0;
 
-for (let i = 1; i < 4; i++) {
-  nomesAlunos[i] = prompt('Digite o nome do ' + i + 'º aluno: ');
-  notas1[i] = parseFloat(prompt('Digite a primeira nota do ' + i + 'º aluno: '));
-  notas2[i] = parseFloat(prompt('Digite a segunda nota do ' + i + 'º aluno: '));
-  mediasAlunos[i] = (notas1[i] + notas2[i]) / 2;
-  mediasSoma = mediasSoma + mediasAlunos[i];
+// for (let i = 1; i < 4; i++) {
+//   nomesAlunos[i] = prompt('Digite o nome do ' + i + 'º aluno: ');
+//   notas1[i] = parseFloat(prompt('Digite a primeira nota do ' + i + 'º aluno: '));
+//   notas2[i] = parseFloat(prompt('Digite a segunda nota do ' + i + 'º aluno: '));
+//   mediasAlunos[i] = (notas1[i] + notas2[i]) / 2;
+//   mediasSoma = mediasSoma + mediasAlunos[i];
+// }
+
+// mediaTurma = mediasSoma / 3;
+
+// console.log('----------------');
+// console.log('LISTAGEM DE ALUNOS');
+// console.log('----------------');
+
+// for (let i = 1; i < 4; i++) {
+//   if (mediasAlunos[i] > mediaTurma) {
+//     alunosAcimaMedia = alunosAcimaMedia + 1;
+//   }
+
+//   console.log(nomesAlunos[i] + ': ' + mediasAlunos[i] + '\n');
+// }
+
+// console.log('----------------');
+// console.log('MÉDIA DA TURMA');
+// console.log('----------------');
+// console.log('A média total da turma é: ' + mediaTurma);
+// console.log('Total de alunos acima da média da turma: ' + alunosAcimaMedia);
+
+// prática 2: apenas letra C
+// let nome = '';
+// let nomeMaiuscula = '';
+// let totalApenasLetraC = 0;
+// const apenasLetraC = [];
+
+// for (let i = 0; i < 3; i++) {
+//   nome = prompt('Digite seu nome: ');
+//   nomeMaiuscula = nome.toUpperCase();
+
+//   if (nomeMaiuscula.startsWith('C')) {
+//     apenasLetraC.push(nome);
+
+//     totalApenasLetraC = totalApenasLetraC + 1;
+//   }
+// }
+
+// console.log('----------------');
+// console.log('LISTAGEM DE NOMES QUE COMEÇAM COM "C"');
+// console.log('----------------');
+
+// for (let i = 0; i < totalApenasLetraC; i++) {
+//   console.log(apenasLetraC[i] + '\n');
+// }
+
+// prática 3: ordenação do vetor
+const vetor = [];
+
+for (let i = 0; i < 4; i++) {
+  vetor[i] = parseInt(prompt('Digite um valor: '));
 }
-
-mediaTurma = mediasSoma / 3;
-
-console.log('----------------');
-console.log('LISTAGEM DE ALUNOS');
-console.log('----------------');
-
-for (let i = 1; i < 4; i++) {
-  if (mediasAlunos[i] > mediaTurma) {
-    alunosAcimaMedia = alunosAcimaMedia + 1;
-  }
-
-  console.log(nomesAlunos[i] + ': ' + mediasAlunos[i] + '\n');
-}
-
-console.log('----------------');
-console.log('MÉDIA DA TURMA');
-console.log('----------------');
-console.log('A média total da turma é: ' + mediaTurma);
-console.log('Total de alunos acima da média da turma: ' + alunosAcimaMedia);
